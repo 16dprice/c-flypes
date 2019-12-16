682ad7d15a1a17f8a44f48bdcf7118b4fd9653f2
DJ <djpric91@gmail.com>
2019-12-16 11:26:11 -0600

    testing flypes

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 
 #include <time.h>
 
+#include "plcTopology.h"
 #include "src/utilities.h"
 #include "src/queue.h"
 #include "src/print_structures.h"
@@ -18,8 +19,13 @@ int main() {
     pd_code_t *next_pd_code;
     pd_crossing_t *crossing;
 
+    char cwd[PATH_MAX];
+    if(getcwd(cwd, sizeof(cwd)) != NULL) {
+        printf("Current working dir: %s\n", cwd);
+    }
+
     // NOTE: the code starts not working for knots of 16 crossings
-    infile = fopen("knot_txt_files/knot_6_1.txt", "r");
+    infile = fopen("/home/dj/CLionProjects/c-flypes/knot_txt_files/knot_6_3.txt", "r");
     next_pd_code = pd_read_KnotTheory(infile);
 
     int cr_num = next_pd_code->ncross;
@@ -71,14 +77,56 @@ int main() {
 //        printf("%s ", is_flype_parallel(cr_num, pd_code, all_flypes.flypes[i]) ? "T" : "F");
 //    }
 
+    pd_stor_t *flyped_codes;
+    pd_code_t *new_pd_code_t;
+
+    flyped_codes = pd_new_pdstor();
+//    new_pd_code_t = pd_code_new((pd_idx_t) cr_num + 2);
+    new_pd_code_t = pd_copy(next_pd_code);
+
     int new_pd_code[cr_num][4];
 
+    pd_addto_pdstor(flyped_codes, next_pd_code, DIAGRAM_ISOTOPY);
     for(int i = 0; i < all_flypes.num_flypes; i++) {
-        anti_parallel_flype(cr_num, pd_code, all_flypes.flypes[i], new_pd_code);
-        print_pd_code(cr_num, new_pd_code);
+        if(!is_flype_parallel(cr_num, pd_code, all_flypes.flypes[i])) {
+            anti_parallel_flype(cr_num, pd_code, all_flypes.flypes[i], new_pd_code);
+            int_array_to_pd_code_t(cr_num, new_pd_code, new_pd_code_t);
+            pd_addto_pdstor(flyped_codes, new_pd_code_t, DIAGRAM_ISOTOPY);
+            print_pd_code(cr_num, new_pd_code);
+        }
     }
 
+    outfile = fopen("/home/dj/CLionProjects/c-flypes/flype_output/knot_6_3.txt", "w");
+    for(next_pd_code = pd_stor_firstelt(flyped_codes); next_pd_code != NULL; next_pd_code = pd_stor_nextelt(flyped_codes)) {
+        pd_write_KnotTheory(outfile, next_pd_code);
+        free(next_pd_code);
+    }
+//    fclose(infile);
+//    fclose(outfile);
+//
+//    flyped_codes = pd_new_pdstor();
+//    infile = fopen("/home/dj/CLionProjects/c-flypes/flype_output/knot_6_1.txt", "r");
+//    for(int i = 0; i < 7; i++) {
+//        next_pd_code = pd_read_KnotTheory(infile);
+//        pd_addto_pdstor(flyped_codes, next_pd_code, DIAGRAM_ISOTOPY);
+//        free(next_pd_code);
+//    }
+//
+//    outfile = fopen("/home/dj/CLionProjects/c-flypes/flype_output/knot_6_1.txt", "w");
+//    for(next_pd_code = pd_stor_firstelt(flyped_codes); next_pd_code != NULL; next_pd_code = pd_stor_nextelt(flyped_codes)) {
+//        pd_write_KnotTheory(outfile, next_pd_code);
+//        free(next_pd_code);
+//    }
+
+
+//    anti_parallel_flype(cr_num, pd_code, all_flypes.flypes[all_flypes.num_flypes - 1], new_pd_code);
+//    print_pd_flype(cr_num, pd_code, all_flypes.flypes[all_flypes.num_flypes - 1]);
+//    print_pd_code(cr_num, new_pd_code);
+
     fclose(infile);
+    fclose(outfile);
+
+    pd_free_pdstor(&flyped_codes);
 
     return 0;
 
